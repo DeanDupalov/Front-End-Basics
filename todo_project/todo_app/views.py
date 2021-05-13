acac5b03f55aff03620bd2d527a96c0d453e07d9
Dean Dupalov <75751527+DeanDupalov@users.noreply.github.com>
2021-05-13 14:11:18 +0100

    basic django todo app

---
+++ todo_project/todo_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from todo_app.models import Todo
+
+
+# Create your views here.
+def index(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, 'todo/index.html', context=context)
